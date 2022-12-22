18a1061e8f0b1c0802bbd37c90ed135771fa0089
Francisco Araujo <110355637+franaraujo3@users.noreply.github.com>
2022-12-21 22:34:44 -0400

    Add files via upload

---
+++ Comandos_basicos/Exercicio18.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int i, num, sum=0;
+	
+	for(i=0; i < 5 ;i++){
+		printf("Insira um numero: ");
+		scanf("%d", &num);
+		if(num % 2 != 0){	
+			if(num > 100 && num < 200){
+				sum += num;
+			}
+		}
+	}
+	printf("%d", sum);
+	
+	return 0;
+}
