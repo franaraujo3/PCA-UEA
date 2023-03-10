c7561a6aaffa0d1ad0d5713875d8e5cc2972210c
Francisco Araujo <110355637+franaraujo3@users.noreply.github.com>
2023-03-09 22:34:08 -0400

    Update 20230307_001.cpp

--- ponteiros 1/20230307_001.cpp
+++ ponteiros 1/20230307_001.cpp
@@ -6,22 +6,22 @@ void print_vector(float *first, float *last){
 	}
 }
 int main(){
-	float vector[5] = {1.0, 2.0, 3.0, 8.0,9.0}, vet2 [5] = {2.1, 2.2, 2.3. 8.8,8.9}, vet3 [5] = {3.1, 3.2, 3.3,8.7,9.8};
+	float vector[5] = {1.0, 2.0, 3.0, 8.0,9.0}, vector2[5] = {2.1, 2.2, 2.3. 8.8,8.9}, vector3[5] = {3.1, 3.2, 3.3,8.7,9.8};
           
     printf("1a parte do vetor 1: ");
-    print_vector(&vet1[0], &vet1[3]);
+    print_vector(&vector[0], &vector[3]);
     printf("2a parte do vetor 1: ");
-    print_vector(&vet1[4], &vet1[4]);
+    print_vector(&vector[4], &vector[4]);
     printf("\n");
     printf("1a parte do vetor 2: ");
-    print_vector(&vet2[0], &vet2[3]);
+    print_vector(&vector2[0], &vector2[3]);
     printf("2a parte do vetor 2: ");
-    print_vector(&vet2[4], &vet2[4]);
+    print_vector(&vector2[4], &vector2[4]);
     printf("\n");
     printf("1a parte do vetor 3: ");
-    print_vector(&vet3[0], &vet3[3]);
+    print_vector(&vector3[0], &vector3[3]);
     printf("2a parte do vetor 3: ");
-    print_vector(&vet3[4], &vet3[4]);
+    print_vector(&vector3[4], &vector3[4]);
  
     return 0;
 }
